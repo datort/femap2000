e3ffec1bdda2a0099e975f91b9b4cad50daff39d
Torsten Schmitz <ts@cossnet.de>
2023-11-08 21:53:09 +0100

    reconfiguring for pico

--- femap-keypad/femap-keypad.ino
+++ femap-keypad/femap-keypad.ino
@@ -1,6 +1,6 @@
 /**
  * 
- * Keymap: 
+ * Keymap D1 Mini Clone: 
  * 
  * D0 - C1
  * D5 - R1
@@ -9,14 +9,28 @@
  * D8 - R3
  * D1 - R4
  * D2 - C3
+ *************
+ * Keymap Pico: 
+ * 
+ * D6 - C1
+ * D10 - R1
+ * D7 - C2
+ * D11 - R2
+ * D12 - R3
+ * D13 - R4
+ * D8 - C3
  */
 
 
-#define NUM_COLUMNS 3
+#define NUM_COLUMNS 3  
 #define NUM_ROWS 4
 
+/* D1 Mini Clone: 
 int columns[NUM_COLUMNS] = { D0, D6, D2 };
-int rows[NUM_ROWS] = { D5, D7, D8, D1 };
+int rows[NUM_ROWS] = { D5, D7, D8, D1 };*/
+
+int columns[NUM_COLUMNS] = { D6, D7, D8 };
+int rows[NUM_ROWS] = { D10, D11, D12, D13 };
 
 int key[NUM_COLUMNS][NUM_ROWS] = {
   { 1, 4, 7, -1 },
@@ -27,14 +41,14 @@ int key[NUM_COLUMNS][NUM_ROWS] = {
 int reading = -99;
 
 void setup() {
-  pinMode(D0, OUTPUT);
   pinMode(D6, OUTPUT);
-  pinMode(D2, OUTPUT);
+  pinMode(D7, OUTPUT);
+  pinMode(D8, OUTPUT);
 
-  pinMode(D5, INPUT);
-  pinMode(D7, INPUT);
-  pinMode(D8, INPUT);
-  pinMode(D1, INPUT);
+  pinMode(D10, INPUT);
+  pinMode(D11, INPUT);
+  pinMode(D12, INPUT);
+  pinMode(D13, INPUT);
 
   Serial.begin(115200);
 }
